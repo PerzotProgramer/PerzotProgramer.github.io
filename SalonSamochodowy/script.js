a08ff4d0ed0f07daa5a2fe84429b191d87e1bfda
Perzot Programista <bartoszperz2007@gmail.com>
2023-05-23 20:44:26 +0200

    Poprawiono formatowanie

--- SalonSamochodowy/script.js
+++ SalonSamochodowy/script.js
@@ -50,12 +50,12 @@ function selection(){
                 }
                 break
 
-                case "bmw":
-                    var divs = document.getElementsByClassName("cars");
-                    for (i = 0; i < divs.length; i++){
-                        if (divs[i].classList.contains("bmw")) divs[i].style.display = "block";
-                        else divs[i].style.display = "none";
-                    }
-                    break
+            case "bmw":
+                var divs = document.getElementsByClassName("cars");
+                for (i = 0; i < divs.length; i++){
+                    if (divs[i].classList.contains("bmw")) divs[i].style.display = "block";
+                    else divs[i].style.display = "none";
+                }
+                break
             }
 }
