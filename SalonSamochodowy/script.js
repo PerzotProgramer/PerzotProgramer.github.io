79a0bbd6801339be712ab66161a7383d11e73c36
Perzot Programista <bartoszperz2007@gmail.com>
2023-05-23 16:56:58 +0200

    Dokończenie skryptu

--- SalonSamochodowy/script.js
+++ SalonSamochodowy/script.js
@@ -1,16 +1,61 @@
 function selection(){
     var select = document.getElementById("cars");
     var value = select.value;
-    if(value == "all"){
-        document.getElementById("bmw").style.opacity = 100;
-        document.getElementById("audi").style.opacity = 100;
-    }
-    else if(value == "bmw"){
-        document.getElementById("bmw").style.opacity = 100;
-        document.getElementById("audi").style.opacity = 0;
-    }
-    else if(value == "audi"){
-        document.getElementById("audi").style.opacity = 100;
-        document.getElementById("bmw").style.opacity = 0;
-    }
+
+    switch (value){
+        case "all":
+            var divs = document.getElementsByClassName("cars");
+                for (i = 0; i < divs.length; i++){
+                    divs[i].style.display = "block";
+                }
+            break
+
+            case "volkswagen":
+                var divs = document.getElementsByClassName("cars");
+                for (i = 0; i < divs.length; i++){
+                    if (divs[i].classList.contains("volkswagen")) divs[i].style.display = "block";
+                    else divs[i].style.display = "none";
+                }
+                break
+
+            case "opel":
+                var divs = document.getElementsByClassName("cars");
+                for (i = 0; i < divs.length; i++){
+                    if (divs[i].classList.contains("opel")) divs[i].style.display = "block";
+                    else divs[i].style.display = "none";
+                }
+                break
+
+            case "mercedes":
+                var divs = document.getElementsByClassName("cars");
+                for (i = 0; i < divs.length; i++){
+                    if (divs[i].classList.contains("mercedes")) divs[i].style.display = "block";
+                    else divs[i].style.display = "none";
+                }
+                break
+
+            case "audi":
+                var divs = document.getElementsByClassName("cars");
+                for (i = 0; i < divs.length; i++){
+                    if (divs[i].classList.contains("audi")) divs[i].style.display = "block";
+                    else divs[i].style.display = "none";
+                }
+                break
+
+            case "porche":
+                var divs = document.getElementsByClassName("cars");
+                for (i = 0; i < divs.length; i++){
+                    if (divs[i].classList.contains("porche")) divs[i].style.display = "block";
+                    else divs[i].style.display = "none";
+                }
+                break
+
+                case "bmw":
+                    var divs = document.getElementsByClassName("cars");
+                    for (i = 0; i < divs.length; i++){
+                        if (divs[i].classList.contains("bmw")) divs[i].style.display = "block";
+                        else divs[i].style.display = "none";
+                    }
+                    break
+            }
 }
