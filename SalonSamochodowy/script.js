cd591f1364e904a3f7e0c6d1e9803f6fcee180ad
Perzot Programista <bartoszperz2007@gmail.com>
2023-05-24 21:01:23 +0200

    Dodano opcje zakupu (tylko JS bez zapisu danych)

--- SalonSamochodowy/script.js
+++ SalonSamochodowy/script.js
@@ -59,3 +59,61 @@ function selection(){
                 break
             }
 }
+
+function WczytajDane(marka, model, rokProdukcji, konieMech, przebieg, cena, img){
+    sessionStorage.setItem("marka", marka);
+    sessionStorage.setItem("model", model);
+    sessionStorage.setItem("rokProdukcji", rokProdukcji);
+    sessionStorage.setItem("konieMech", konieMech);
+    sessionStorage.setItem("przebieg", przebieg);
+    sessionStorage.setItem("cena", cena);
+    sessionStorage.setItem("img", img);
+    location.href = "zakup.html";
+}
+
+function OdczytajDane(){
+    var marka = sessionStorage.getItem("marka");
+    var model = sessionStorage.getItem("model");
+    var rokProdukcji = sessionStorage.getItem("rokProdukcji");
+    var konieMech = sessionStorage.getItem("konieMech");
+    var przebieg = sessionStorage.getItem("przebieg");
+    var cena = sessionStorage.getItem("cena");
+    var img = sessionStorage.getItem("img");
+    
+    document.getElementById("marka").innerHTML = "Marka: ".concat(marka);
+    document.getElementById("model").innerHTML = "Model: ".concat(model);
+    document.getElementById("rokProdukcji").innerHTML = "Rok Produkcji: ".concat(rokProdukcji);
+    document.getElementById("konieMech").innerHTML = "Moc silnika: ".concat(konieMech);
+    document.getElementById("przebieg").innerHTML = "Przebieg: ".concat(przebieg);
+    document.getElementById("cena").innerHTML = "Cena: ".concat(cena);
+    document.getElementById("img").src = img;
+}
+
+function Wroc(){location.href = "index.html";}
+
+function Potwierdz(){
+    location.href = "potwierdzenie.html";
+}
+
+function Zakup(){
+    var marka = sessionStorage.getItem("marka");
+    var model = sessionStorage.getItem("model");
+    var wiadomosc = "Dziękujemy za zakup samochodu ".concat(marka).concat(" ").concat(model);
+    document.getElementById("wiadomosc").innerHTML = wiadomosc;
+
+    var date = new Date();
+    var dzien = date.getDate();
+    var miesiac = date.getMonth() + 1;
+    var rok = date.getFullYear();
+    dzien = dzien + Math.floor(Math.random() * 21);
+
+    if (dzien >= 31){
+        dzien %= 31;
+        miesiac += 1;
+        if (dzien == 0) dzien += 1;
+    }
+
+
+    var czasOczekiwania = "Przewidywany czas odbioru: ".concat(dzien).concat(".").concat(miesiac).concat(".").concat(rok);
+    document.getElementById("dostawa").innerHTML = czasOczekiwania;
+}
