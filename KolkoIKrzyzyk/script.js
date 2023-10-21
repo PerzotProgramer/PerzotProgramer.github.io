8f0b519ab758a101c89ac199f4b03ca926c8a0b2
PerzotProgrammer <bartoszperz2007@gmail.com>
2023-10-21 18:40:23 +0200

    Zlikwidowano błędy przy klikaniu w tło oraz sformatowano kod

--- KolkoIKrzyzyk/script.js
+++ KolkoIKrzyzyk/script.js
@@ -1,30 +1,26 @@
 let turn = "O";
-let win = false;
-let draw = false;
+let isOver = false;
 let moves = 0;
 
-function Click()
-{
-    if (turn === "O")
-    {
-        window.onclick = e => {
-            if (win || draw) return;
-            if (document.getElementById(e.target.id).innerHTML !== "") return;
-            document.getElementById(e.target.id).innerHTML = turn;
-            document.getElementById(e.target.id).classList.add("O");
+function Click() {
+    if (turn === "O") {
+        window.onclick = clickedElement => {
+            let check = document.getElementById(clickedElement.target.id);
+            if (isOver || check === null || check.innerHTML !== "") return;
+            document.getElementById(clickedElement.target.id).innerHTML = turn;
+            document.getElementById(clickedElement.target.id).classList.add("O");
             turn = "X";
             document.getElementById("info").innerHTML = "Ruch " + turn;
             WinCheck();
             DrawCheck();
         }
     }
-    if (turn === "X")
-    {
-        window.onclick = e => {
-            if (win || draw) return;
-            if (document.getElementById(e.target.id).innerHTML !== "") return;
-            document.getElementById(e.target.id).innerHTML = turn;
-            document.getElementById(e.target.id).classList.add("X");
+    if (turn === "X") {
+        window.onclick = clickedElement => {
+            let check = document.getElementById(clickedElement.target.id);
+            if (isOver || check === null || check.innerHTML !== "") return;
+            document.getElementById(clickedElement.target.id).innerHTML = turn;
+            document.getElementById(clickedElement.target.id).classList.add("X");
             turn = "O";
             document.getElementById("info").innerHTML = "Ruch " + turn;
             WinCheck();
@@ -33,41 +29,34 @@ function Click()
     }
 }
 
-function WinCheck()
-{
+function WinCheck() {
     const tiles = document.getElementsByClassName("tile");
     const WinCombos = [
-        [0,1,2],[3,4,5],[6,7,8],
-        [0,3,6],[1,4,7],[2,5,8],
-        [0,4,8],[2,4,6]
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
     ]
 
     WinCombos.forEach(arr => {
-        const circleWins = arr.every(
-            tile => tiles[tile].classList.contains("O"))
-        if (circleWins) 
-        {
+        const circleWins = arr.every(tile => tiles[tile].classList.contains("O"))
+        if (circleWins) {
             document.getElementById("info").innerHTML = "O Wygrało!";
-            win = true;
+            isOver = true;
         }
     })
     WinCombos.forEach(arr => {
-            const crossWins = arr.every(
-                tile => tiles[tile].classList.contains("X"))
-            if (crossWins) 
-            {
-                document.getElementById("info").innerHTML = "X Wygrał!";
-                win = true;
-            }
+        const crossWins = arr.every(tile => tiles[tile].classList.contains("X"))
+        if (crossWins) {
+            document.getElementById("info").innerHTML = "X Wygrał!";
+            isOver = true;
+        }
     })
 }
 
-function DrawCheck()
-{
-    moves ++;
-    if (moves === 9)
-    {
+function DrawCheck() {
+    moves++;
+    if (moves === 9) {
         document.getElementById("info").innerHTML = "REMIS!";
-        draw = true;
+        isOver = true;
     }
 }
