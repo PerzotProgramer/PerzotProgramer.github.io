a52561baf8aaae9ed40ee3cbac9be24b5cd31988
PerzotProgrammer <bartoszperz2007@gmail.com>
2023-11-18 12:06:25 +0100

    Dopracowano projekt kalkulatora

--- Kalkulator/script.js
+++ Kalkulator/script.js
@@ -3,6 +3,8 @@ let displayText = "";
 let Chars = ['+', '-', '*', '/'];
 
 function GetButton(which) {
+    ChangeSize();
+    if (!IsThereSpace(displayText)) return;
     if (displayText === "" && which === '0') return;
     if (displayText === "" && Chars.includes(which)) displayText += '0' + which;
     else displayText += which;
@@ -10,8 +12,10 @@ function GetButton(which) {
 }
 
 function Calculate() {
+    ChangeSize();
     try {
         let output = eval(displayText);
+        if (!IsThereSpace(output)) return;
         if (output === Infinity || (isNaN(output) && displayText !== "")) {
             output = "<span class='smallText'>Nie można dzielić przez 0!</span>";
         } else if (output === undefined) {
@@ -28,4 +32,23 @@ function ClearInputAndOutput() {
     displayText = "0";
     document.getElementById("outputText").innerHTML = displayText;
     displayText = "";
+}
+
+function IsThereSpace(inThisString) {
+    if (inThisString.length >= 30) {
+        document.getElementById("outputText").innerHTML = "Za dużo znaków!";
+        return false;
+    }
+    return true;
+}
+
+function ChangeSize() {
+    window.onclick = clickedElement => {
+        let element = document.getElementById(clickedElement.target.id);
+        if (!element.classList.contains("box")) return;
+        element.style.transform = "scale(95%)";
+        setTimeout(function () {
+            element.style.transform = "scale(100%)";
+        }, 100);
+    }
 }
