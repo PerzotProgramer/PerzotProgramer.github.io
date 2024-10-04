f7aabf76acd2ed548236349b95f457b3d19519bd
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-10-04 14:56:41 +0200

    Dodano funkcjonalność w jQuerable

--- jQuerable/script.js
+++ jQuerable/script.js
@@ -1,26 +1,81 @@
-const words = "WAGON"
+const words = [
+    "WAGON",
+    "PTAKI",
+    "ALBUM",
+    "ATUTY",
+    "WAWER",
+    "KOLOR",
+    "ROWER",
+    "BANAN",
+    "FILMY",
+    "FANTA",
+    "PEPSI",
+]; // Będzie ich więcej
+const word = setRandomWord();
+let invalidLetters = [];
 let lineNumber = 0;
-let winCondition = false;
+let isGameOver = false;
+
+function setRandomWord() {
+    return words[Math.floor(Math.random() * words.length)];
+}
+
+function handleInput(input) {
+    if (isGameOver) return "";
+    input = input.toUpperCase();
+    if (invalidLetters.includes(input[input.length - 1]) || !input[input.length - 1].match(/[A-Za-z]/)) {
+        input = input.slice(0, -1);
+        return input;
+    }
+    return input;
+}
 
 function check() {
-    if (winCondition) return;
-    const userInput = document.getElementById("userInput").value;
-    if (userInput.length < 5) return;
+    if (isGameOver) return;
+    const info = $("#info");
+    const userInput = $("#userInput").val();
+    if (userInput.length < 5) {
+        info.text("Write at least 5 letters!");
+        return;
+    } else info.text("");
     let wordIndex = 0;
     let hitLetters = 0;
     $(`#rowId${lineNumber}`).children().each(function () {
         const cell = $(this);
-        if (userInput[wordIndex] === words[wordIndex]) {
+        if (userInput[wordIndex] === word[wordIndex]) {
             cell.addClass("gridCellCorrectSpot");
             hitLetters++;
-        } else if (words.includes(userInput[wordIndex])) {
+        } else if (word.includes(userInput[wordIndex])) {
             cell.addClass("gridCellCorrectLetter");
         } else {
             cell.addClass("gridCellWrong");
+            if (!invalidLetters.includes(userInput[wordIndex])) {
+                invalidLetters.push(userInput[wordIndex]);
+            }
         }
         cell.text(userInput[wordIndex]);
         wordIndex++;
     });
-    if (hitLetters === 5) winCondition = true;
+    updateUsedLetters();
+    $("#userInput").val("");
+    if (hitLetters === 5) {
+        isGameOver = true;
+        InformationDisplay("YOU WIN!");
+        return;
+    }
+    if (lineNumber === 4) {
+        isGameOver = true;
+        InformationDisplay("YOU LOST!")
+        return;
+    }
     lineNumber++;
+}
+
+function updateUsedLetters() {
+    const sortedLetters = invalidLetters.sort().toString().replaceAll(",", " ");
+    $("#usedLetters").text(sortedLetters);
+}
+
+function InformationDisplay(info) {
+    $("#info").text(info);
 }
